9c97f75b3ed2ed770e89bd50f195c5a21c9062d3
Venkatesh <76658782+venkiganesh@users.noreply.github.com>
2021-11-14 20:08:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,54 @@
+function s1() {
+  var x = document.getElementById("btn1");
+x.style.backgroundColor = "blue";
+var a = document.getElementById("btn4");
+var b = document.getElementById("btn2");
+var c = document.getElementById("btn3");
+a.style.backgroundColor = "white";
+b.style.backgroundColor = "white";
+c.style.backgroundColor = "white";
+}
+function s2() {
+  var x = document.getElementById("btn2");
+x.style.backgroundColor = "blue";
+var a = document.getElementById("btn4");
+var b = document.getElementById("btn1");
+var c = document.getElementById("btn3");
+a.style.backgroundColor = "white";
+b.style.backgroundColor = "white";
+c.style.backgroundColor = "white";
+
+}
+function s3() {
+  var x = document.getElementById("btn3");
+x.style.backgroundColor = "blue";
+var a = document.getElementById("btn4");
+var b = document.getElementById("btn1");
+var c = document.getElementById("btn2");
+a.style.backgroundColor = "white";
+b.style.backgroundColor = "white";
+c.style.backgroundColor = "white";
+
+}
+function s4() {
+  var x = document.getElementById("btn4");
+x.style.backgroundColor = "blue";
+var a = document.getElementById("btn2");
+var b = document.getElementById("btn1");
+var c = document.getElementById("btn3");
+a.style.backgroundColor = "white";
+b.style.backgroundColor = "white";
+c.style.backgroundColor = "white";
+
+}
+
+function getin() {
+  var x = document.getElementById("con");
+  x.style.display="block";
+
+}
+function getback() {
+  var x = document.getElementById("con");
+  x.style.display="none";
+
+}
